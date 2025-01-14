8e4119a05974e8387ff7a3532c3610830630ae62
Lucaziw <132687467+Lucaziw@users.noreply.github.com>
2025-01-14 16:30:40 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Obs.: Edite o nome da pasta vscode para .vscode
+(Faz-se necessário alterar a rota do arquivo, no launch.json, sendo relativo aonde se localiza o arquivo no pc instalado)
+
+Após baixar o projeto, dê um npm install, para baixar as depêndencias necessárias para rodá-lo em sua máquina.
---
+++ server/routes/agendamentoRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const agendamentoController = require('../controller/agendamentoController');
+
+router.post('/novo', agendamentoController.criarAgendamento);
+router.get('/buscar/:idAgendamento', agendamentoController.buscarAgendamento);
+router.put('/:idAgendamento', agendamentoController.editarAgendamento);
+router.put('/:idAgendamento/cancelar', agendamentoController.cancelarAgendamento);
+
+router.get('/buscar-paciente/:idUser', agendamentoController.buscarPacientePorUserId);
+router.get('/paciente/:idPaciente', agendamentoController.listarAgendamentosPaciente);
+router.get('/paciente/:idPaciente/agendamentos', agendamentoController.buscarAgendamentosPaciente);
+
+router.get('/medico/:idMedico', agendamentoController.listarAgendamentosMedico);
+router.get('/medico/:idMedico/consultas', agendamentoController.buscarConsultasSemanalMedico);
+router.get('/medico/:idMedico/disponibilidade', agendamentoController.buscarDisponibilidade);
+router.get('/buscar-medico/:idUser', agendamentoController.buscarMedicoPorUserId);
+
+router.get('/disponibilidade/:idMedico', agendamentoController.buscarDisponibilidade);
+router.post('/horarios-ocupados', agendamentoController.verificarHorariosOcupados);
+
+router.post('/verificar-horarios-ocupados', agendamentoController.verificarHorariosOcupados);
+router.post('/verificar-disponibilidade', agendamentoController.verificarDisponibilidade);
+
+router.get('agendamentos/especialidades', agendamentoController.listarEspecialidades);
+router.get('/especialidades/:idMedico', agendamentoController.listarEspecialidades);
+router.get('/medicos/:idEspecialidade', agendamentoController.listarMedicosPorEspecialidade);
+
+router.get('/pacientes/:idPaciente/detalhes', agendamentoController.buscarDetalhesPaciente);
+router.put('/:idAgendamento/concluir', agendamentoController.concluirConsulta);
+
+
+module.exports = router;
